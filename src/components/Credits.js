46ff1e6bc0156e0d8b24cc877b7f32e2781bf5be
hayimheron <102389144+hayimheron@users.noreply.github.com>
2024-04-02 13:39:09 -0400

    add CreditsView

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -7,6 +7,13 @@ Note: You need to work on this file for the Assignment.
 import {Link} from 'react-router-dom';
 import AccountBalance from './AccountBalance';
 
+let creditsView = () => {
+  return credits.map((credits) => { 
+    let date = credits.date.slice(0, 10);
+    return <li key={credits.id}>{credits.amount} {credits.description} {date}</li>
+  });
+}
+
 const Credits = (props) => {
   return (
     <div>
