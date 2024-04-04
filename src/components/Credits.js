080093eca9d6e39b1d2d0a1c41c21c488c741292
hayimheron <102389144+hayimheron@users.noreply.github.com>
2024-04-03 23:54:43 -0400

    revert to original in wrong file

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,23 +5,7 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
-import AccountBalance from './AccountBalance';
 
-
-const Debits = (props) => {
-  const { debits, balance, updateAccountBalance } = props;
-  // Initialize list balance
-  let newBalance = 0;
-
-  
-}
-
-  let debitsView = () => {
-    return debits.map((debits) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
-      let date = debits.date.slice(0, 10);
-      return <li key={debits.id}>{debits.amount} {debits.description} {date}</li>
-    });
-  }
 
 const Credits = (props) => {
   return (
